c78a24ebc8a0603b5b2372c3ef8f70e319395871
Chandradeep-pra <chandradeepprasad1998@gmail.com>
2025-08-28 16:07:56 +0530

    trying-not-next-public-deployment

--- app/api/(auth)/check-subscription/route.ts
+++ app/api/(auth)/check-subscription/route.ts
@@ -3,10 +3,20 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 
+// if (getApps().length === 0) {
+//   initializeApp({
+//     credential: cert({
+//       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+//     }),
+//   });
+// }
+
 if (getApps().length === 0) {
   initializeApp({
     credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
